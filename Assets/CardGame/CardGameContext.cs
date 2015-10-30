d36126bc39f9e98281a071a9e22702cd0fd10cd6
naveen janjanam <nvnjls@gmail.com>
2015-10-30 16:45:44 +0530

    First Commit

---
+++ Assets/CardGame/CardGameContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using UnityEngine;
+
+using strange.extensions.context.impl;
+
+namespace strange.examples.CardGame {
+	public class CardGameContext : SignalContext {
+		
+		/**
+         * Constructor
+         */
+		public CardGameContext(MonoBehaviour contextView) : base(contextView) {
+		}
+		
+		protected override void mapBindings() {
+			base.mapBindings();
+			
+			// we bind a command to StartSignal since it is invoked by SignalContext (the parent class) on Launch()
+			commandBinder.Bind<StartSignal>().To<CardGameStartCommand>().Pooled();
+			commandBinder.Bind<CardSelectedSignal>().To<SelectCardCommand>().Pooled();
+			commandBinder.Bind<CheckCardsSignal>().To<CheckCardsCommand>().Pooled();
+			commandBinder.Bind<SetGameModeSignal>().To<SetGameModeCommand>().Pooled();
+			commandBinder.Bind<ShowResultSignal>().To<ShowResultCommand>().Pooled();
+			commandBinder.Bind<ShowWarningSignal>().To<ShowWarningCommand>().Pooled();
+			commandBinder.Bind<UpdatePlayerScoreSignal>().To<UpdatePlayerScoreCommand>().Pooled();
+			commandBinder.Bind<UpdateAIScoreSignal>().To<UpdateAIScoreCommand>().Pooled();
+
+			CardManager manager = GameObject.Find("CanvasObj").GetComponent<CardManager>();
+			injectionBinder.Bind<ICardsManager>().ToValue(manager);
+
+			HUDManager viewmanager = manager.GetComponentInChildren<HUDManager>();
+			injectionBinder.Bind<IHUDViewManager>().ToValue(viewmanager);
+		}
+		
+	}
+}
